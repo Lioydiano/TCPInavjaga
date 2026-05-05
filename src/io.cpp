b52cf1223b0a4cd364b88ca19a8a14864bb26660
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2026-05-06 00:12:56 +0300

    Potentially functioning polling mechanism

--- src/io.cpp
+++ src/io.cpp
@@ -2,11 +2,13 @@
 #include "io.hpp"
 #include <netinet/tcp.h>
 #include <arpa/inet.h>
+#include <exception>
 #include <string.h>
 #include <iostream>
 #include <poll.h>
 
 /** @brief Waits for a message from the other end of the socket
+ * @throws std::runtime_error when the recv call on the file descriptor fails
  * @return A move event representing the received move
  */
 MoveEvent InavjagaGSPIO::recvMove() {
@@ -18,10 +20,11 @@ MoveEvent InavjagaGSPIO::recvMove() {
      * @note every move is made of one character
      */
     // https://stackoverflow.com/questions/71744538/why-would-one-need-to-use-msg-waitall-flag-instead-of-0-flag-why-to-use-it
-    int returnCode = recv(this->socketfd, &buffer, 1+1+1, MSG_WAITALL);
+    int rc = recv(this->socketfd, &buffer, 1+1+1, MSG_WAITALL);
     MoveEvent moveEvent = {INAVJAGA_PLAYER_ID_IGNORE, INAVJAGA_CHAR_MOVE_IGNORE};
-    if (returnCode < 0) {
-        /** @todo throw an exception */
+    if (rc < 0) {
+        std::string errorBuffer = "Scanning a socket that was expected to be empty gave error code " + std::to_string(rc);
+        throw new std::runtime_error(errorBuffer);
     }
     sscanf(buffer, "%hu;%c", &moveEvent.playerId, &moveEvent.move);
     return moveEvent;
@@ -38,13 +41,54 @@ MoveEvent InavjagaGSPIO::recvMove() {
  */
 std::pair<size_t, MoveEvent> InavjagaGSPIO::pollMany(
     const std::vector<std::shared_ptr<InavjagaGSPIO>>& ios, int timeout = 1000) {
-        struct pollfd pollFds[ios.size()];
+        const size_t iosLen = ios.size();
+        struct pollfd* pollFds = (struct pollfd*)calloc(iosLen, sizeof(pollfd));
         bzero(&pollFds, sizeof(pollFds));
-        for (size_t i = 0; i < ios.size(); i++) {
+        for (size_t i = 0; i < iosLen; i++) {
             pollFds[i].fd = ios[i]->socketfd;
             pollFds[i].events = POLLIN;
         }
-        // TODO
+        int rc = poll(pollFds, iosLen, timeout);
+        if (rc <= 0) {
+            if (rc < 0) {
+                std::cerr << "poll() failed with code " << rc << std::endl;
+                /** @note We should definitely log this,
+                 * but it can fail without throwing for now,
+                 * we just don't need it outside debugging
+                 */
+            }
+            return std::make_pair(
+                INAVJAGA_PLAYER_ID_IGNORE,
+                MoveEvent{
+                    INAVJAGA_PLAYER_ID_IGNORE,
+                    INAVJAGA_CHAR_MOVE_IGNORE
+                }
+            );
+        }
+        for (size_t i = 0; i < iosLen; i++) {
+            if (pollFds[i].revents & POLLHUP) { // If the client disconnected...
+                return std::make_pair(i, MoveEvent{(player_id_t)i, 'Q'});
+            } else if (pollFds[i].revents & POLLIN) { // If the client sent something...
+                try {
+                    MoveEvent moveEvent = ios[i]->recvMove();
+                    return std::make_pair(i, moveEvent);
+                } catch (std::exception& e) {
+                    std::cerr << e.what() << std::endl;
+                    /** @note We should definitely log this,
+                     * but it can fail without throwing for now,
+                     * we just don't need it outside debugging
+                     */
+                }
+            }
+        }
+        free(pollFds);
+        return std::make_pair(
+            INAVJAGA_PLAYER_ID_IGNORE,
+            MoveEvent{
+                INAVJAGA_PLAYER_ID_IGNORE,
+                INAVJAGA_CHAR_MOVE_IGNORE
+            }
+        );
 }
 
 MoveEvent LocalInavjagaIO::getMove() {
