607d718fb7d5f7287df9bdf3ab160b2e1409f214
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2026-05-26 14:34:17 +0300

    Serializing Chest

--- src/serialize.cpp
+++ src/serialize.cpp
@@ -1,6 +1,7 @@
 #include "serialize.hpp"
 #include "archer.hpp"
 #include "bullet.hpp"
+#include "chest.hpp"
 #include <sstream>
 
 extern std::mutex streamMutex;
@@ -12,31 +13,44 @@ std::string serializeGameState() {
     std::string serialized;
     serialized.append("Archers:");
     for (std::shared_ptr<Archer> archer : Archer::archers) {
-        serialized.append(serializeArcher(archer));
+        serialized.append(serialize(archer));
         if (archer != Archer::archers.back())
             serialized.append(";");
     }
     serialized.append(classTermination);
     for (std::shared_ptr<Bullet> bullet : Bullet::bullets) {
-        serialized.append(serializeBullet(bullet));
+        serialized.append(serialize(bullet));
         if (bullet != Bullet::bullets.back())
             serialized.append(";");
     }
     serialized.append(classTermination);
+    for (std::shared_ptr<Chest> chest : Chest::chests) {
+        serialized.append(serialize(chest));
+        if (chest != Chest::chests.back())
+            serialized.append(";");
+    }
+    serialized.append(classTermination);
+
     /// @todo All the other parts that should be provided
 }
 
-std::string serializeArcher(std::shared_ptr<Archer> archer) {
+std::string serialize(std::shared_ptr<Archer> archer) {
     return serializeCoordinates(archer->getCoordinates());
 }
 
-std::string serializeBullet(std::shared_ptr<Bullet> bullet) {
+std::string serialize(std::shared_ptr<Bullet> bullet) {
     std::ostringstream os(serializeCoordinates(bullet->getCoordinates()));
     os << ',' << bullet->collided << ',' << bullet->direction;
     return os.str();
 }
 
-std::string serializeCoordinates(sista::Coordinates coordinates) {
+std::string serialize(std::shared_ptr<Chest> chest) {
+    std::ostringstream os(serializeCoordinates(chest->getCoordinates()));
+    os << ",{" << chest->inventory.clay << "," << chest->inventory.bullets << "," << chest->inventory.meat << "}";
+    return os.str();
+}
+
+std::string serialize(sista::Coordinates coordinates) {
     std::ostringstream os;
     os << '{' << coordinates.y << ',' << coordinates.x << '}';
     return os.str();
--- src/serialize.hpp
+++ src/serialize.hpp
@@ -1,13 +1,15 @@
 #include "io.hpp"
 #include "archer.hpp"
 #include "bullet.hpp"
+#include "chest.hpp"
 #include <sista/coordinates.hpp>
 
 std::string serializeCoordinates(sista::Coordinates);
 std::string serializeCoordinates(unsigned short, unsigned short);
 
-std::string serializeArcher(std::shared_ptr<Archer>);
-std::string serializeBullet(std::shared_ptr<Bullet>);
+std::string serialize(std::shared_ptr<Archer>);
+std::string serialize(std::shared_ptr<Bullet>);
+std::string serialize(std::shared_ptr<Chest>);
 
 std::string serializeGameState();
 void sendGameState(std::shared_ptr<ServerInavjagaGSPIO>);
